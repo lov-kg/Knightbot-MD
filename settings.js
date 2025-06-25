43aaeae1ce0517d54140ee61ffdb79d434dc401c
lov-kg <kagisomatjeke69@gmail.com>
2025-06-25 12:51:12 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -3,7 +3,7 @@ const settings = {
   author: '‎',
   botName: "Kagisos wingman",
   botOwner: 'kagiso', // Your name
-  ownerNumber: '27765980111', //Set your number here without + symbol, just add country code & number without any space
+  ownerNumber: '+27765980111', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
